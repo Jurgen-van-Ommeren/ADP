31e0d91b681616b723452dd2f96be32d0efb4720
Jurgen van Ommeren <jurgenvanommeren@gmail.com>
2024-11-28 09:56:31 +0100

    commit pizza object

---
+++ ADP/ADP/TestObjects/Pizza.cs
@@ -0,0 +1,22 @@
+﻿namespace ADP.TestObjects;
+
+public class Pizza : IComparable<Pizza>
+{
+    public string Name { get; set; }
+    public int Slices { get; set; }
+
+    public int CompareTo(Pizza comparingPizza)
+    {
+        if (Slices > comparingPizza.Slices)
+        {
+            return 1;
+        }
+
+        if (Slices < comparingPizza.Slices)
+        {
+            return -1;
+        }
+
+        return string.Compare(Name, comparingPizza.Name, StringComparison.Ordinal);
+    }
+}
