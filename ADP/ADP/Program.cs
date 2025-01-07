c93dd1b94d188f7fdb77e5cfc685f207b96da519
Dinant de Wit <73031899+witje@users.noreply.github.com>
2025-01-07 20:01:12 +0100

    Quicksort tests

--- ADP/ADP/Program.cs
+++ ADP/ADP/Program.cs
@@ -1,15 +1,12 @@
 ﻿using System.Text.Json;
 using ADP.Dataset;
 using ADP.Operations.Deque;
-using ADP.Sorting.MergeSort;
+using ADP.Sorting.MergeSort;using ADP.Sorting.QuickSort;
 using ADP.TestObjects;
 
 var datasetSorting = JsonSerializer.Deserialize<DatasetSorting>(File.ReadAllText("Dataset/dataset_sorteren.json"));
 
 datasetSorting.Pizzas = PizzaGenerator.GenerateRandomPizzas(8001);
 
-int[] array = { 3, 1, 2, 4, 6, 5, 8, 7, 10, 9 };
-
-MergeSort<int>.Sort(array);
-
-var result = array;
+new QuickSortTest()
+    .Run(datasetSorting);
